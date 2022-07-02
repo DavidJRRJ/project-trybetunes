650ca0c5662901ff84b9a9d2bbd53dc3d7ab1262
David Junior <david.lourenco94@gmail.com>
2022-07-02 16:49:00 -0300

    Requisito 05

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,43 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isButtonDisabled: true,
+      valuePesq: '',
+    };
+  }
+
+  nameCondition = () => {
+    const { valuePesq } = this.state;
+    const letrasMin = 2;
+    this.setState({ isButtonDisabled: valuePesq.length < letrasMin });
+  }
+
+  handleChange = ({ target }) => {
+    const { value } = target;
+    this.setState({ valuePesq: value }, this.nameCondition);
+  }
+
   render() {
+    const { valuePesq, isButtonDisabled } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <p>search</p>
+        <input
+          data-testid="search-artist-input"
+          type="text"
+          value={ valuePesq }
+          onChange={ this.handleChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ isButtonDisabled }
+        >
+          Procurar
+        </button>
       </div>
     );
   }
